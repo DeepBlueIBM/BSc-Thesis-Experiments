d8964b165c95b65ab60384915f6806ff0a7219df
GEORGIOS KOTSIFOS <54845904+GKotsifos@users.noreply.github.com>
2023-09-24 21:17:04 +0300

    Update Algorithm_Executioner.py

--- Algorithm_Executioner.py
+++ Algorithm_Executioner.py
@@ -1,6 +1,8 @@
+# -- pathname x_workers_x_tasks
 import pandas as pd
 import xlsxwriter
 import argparse
+import time
 
 from algorithms.LMIS_deletion import A
 from algorithms.LMIS_included import Algo
@@ -8,6 +10,8 @@ from algorithms.IntegerProgram import IP
 import ast
 import os
 import csv
+
+start_time = time.time()
 
 experimental_results = []
 
@@ -71,32 +75,45 @@ class AlgorithmExecutioner:
 
     def run(self, path_name, file_name):
         num_workers = int(path_name.split('_')[0].split('workers')[0])
-        num_tasks = int(path_name.split('_')[2].split('tasks')[0])
+        # num_tasks = int(path_name.split('_')[2].split('tasks')[0])
 
         sol, cost = self.P1.run()
-        order, final_sol, final_cos = self.P2.run()
-        opt_values, opt_cost = self.P3.solve()
-        print("---------- Solution for instance: ", file_name, "----------")
         if sol != 0 and cost != 0:
+            order, final_sol, final_cos, delta, max_delta_index = self.P2.run()
+            opt_values, opt_cost = self.P3.solve()
+            print("---------- Solution for instance: ", file_name, "----------")
             print("LMIS solution is:", sol)
             print("The cost is:", cost)
             print("Order of selection is:", order)
             print("LMIS solution after deletion is:", final_sol)
             print("The final cost after deletion is:", final_cos)
+            print("Δ is:", delta)
+            print("Max Δ task is:", max_delta_index)
+
+            for i in range(len(opt_values)):
+                if opt_values[i] > 0:
+                    opt_values[i] = i + 1
+
+            opt_values = [i for i in opt_values if i != 0]
+
+            if opt_values is not None and opt_cost is not None:
+                print("Optimal selection is:", opt_values)
+                print("Optimal cost:", opt_cost)
         else:
-            print("Infeasible Solution for this instance")
+            print("Infeasible Solution for instance: ", file_name)
 
-        for i in range(len(opt_values)):
-            if opt_values[i] > 0:
-                opt_values[i] = i + 1
+        #print("________________", opt_values, opt_cost)
+        #for i in range(len(opt_values)):
+        #    if opt_values[i] > 0:
+        #        opt_values[i] = i + 1
 
-        opt_values = [i for i in opt_values if i != 0]
+        #opt_values = [i for i in opt_values if i != 0]
 
-        if opt_values is not None and opt_cost is not None:
-            print("Optimal selection is:", opt_values)
-            print("Optimal cost:", opt_cost)
+        #if opt_values is not None and opt_cost is not None:
+            #print("Optimal selection is:", opt_values)
+            #print("Optimal cost:", opt_cost)
 
-        save_results(cost, final_cos, opt_cost, path_name, file_name, num_workers, num_tasks)
+        # save_results(cost, final_cos, opt_cost, path_name, file_name, num_workers, num_tasks)
 
 
 parser = argparse.ArgumentParser(description='Algorithm Execution')
@@ -105,10 +122,13 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     filenames = ['instance01.csv', 'instance02.csv', 'instance03.csv', 'instance04.csv', 'instance05.csv',
-                 'instance06.csv', 'instance07.csv', 'instance08.csv', 'instance09.csv', 'instance10.csv']
+                 'instance06.csv', 'instance07.csv', 'instance08.csv', 'instance09.csv', 'instance10.csv',
+                 'instance11.csv', 'instance12.csv', 'instance13.csv', 'instance14.csv', 'instance15.csv',
+                 'instance16.csv', 'instance17.csv', 'instance18.csv', 'instance19.csv', 'instance20.csv']
     path_name = args.path_name
     for filename in filenames:
-        dataset_path = os.path.join(os.path.dirname(__file__), 'datasets\\' + path_name, filename)
+        dataset_path = os.path.join(os.path.dirname(__file__), 'data\\', path_name,
+                                    filename)  # 'data\\' + path_name, filename
         with open(dataset_path) as file:
             reader = csv.DictReader(file)
             b = []
@@ -123,7 +143,7 @@ if __name__ == '__main__':
 
             for row in reader:
                 if row['contribution'] != '':
-                    q.append(int(row['contribution']))
+                    q.append(float(row['contribution']))
             file.seek(0)  # reset file pointer
             next(reader)
 
@@ -133,3 +153,7 @@ if __name__ == '__main__':
 
         main_program = AlgorithmExecutioner(b, q, d)
         main_program.run(path_name, filename)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Elapsed time: ", elapsed_time)
